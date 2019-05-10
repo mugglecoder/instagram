5a8134b633b5620c47e22c1cf5a72e60f74162c3
Park <winkknd@naver.com>
2019-05-11 00:30:41 +0900

    ...

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -3,7 +3,6 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Mutation: {
     createAccount: async (_, args) => {
-      console.log(args);
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
       const user = await prisma.createUser({
         username,
