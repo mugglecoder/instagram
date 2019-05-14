2c517d33e44c48b90260d5a9cfbf10cae4f590f6
Park <winkknd@naver.com>
2019-05-15 05:07:46 +0900

    #3.5 Passport JWT part Two

--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -1,14 +1,13 @@
 import { prisma } from "../../../../generated/prisma-client";
+import { generateToken } from "../../../utils";
 
 export default {
   Mutation: {
     confirmSecret: async (_, args) => {
       const { email, secret } = args;
       const user = await prisma.user({ email });
-      console.log(user);
       if (user.loginSecret === secret) {
-        //JWT
-        return "TOKEN";
+        return generateToken(user.id);
       } else {
         throw Error("비번 조합이 잘못됨");
       }
