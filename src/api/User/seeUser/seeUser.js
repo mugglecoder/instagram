2d56d69cfbd9714a0f0b38cc9007759bada14f50
Park <winkknd@naver.com>
2019-05-18 18:13:28 +0900

    #3.12 me Resolver + Prisma's Limitations

--- src/api/User/seeUser/seeUser.js
+++ src/api/User/seeUser/seeUser.js
@@ -3,7 +3,6 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Query: {
     seeUser: async (_, args, { request }) => {
-      const { user } = request;
       const { id } = args;
       return prisma.user({ id });
     }
