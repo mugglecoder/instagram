6b2dbd82562f0d2d9edb1a7d5b279ac71aba9288
Park <winkknd@naver.com>
2019-05-18 23:06:26 +0900

    #3.16 itsMe and amIFollowing Fileds part Two

---
+++ src/api/User/computed.js
@@ -0,0 +1,25 @@
+import { prisma } from "../../../generated/prisma-client";
+
+export default {
+  User: {
+    fullName: parent => {
+      return `${parent.firstName} ${parent.lastName}`;
+    },
+    isFollowing: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      try {
+        return await prisma.$exists.user({
+          AND: [{ id: user.id }, { following_some: { id: parentId } }]
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    isSelf: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      return user.id === parentId;
+    }
+  }
+};
