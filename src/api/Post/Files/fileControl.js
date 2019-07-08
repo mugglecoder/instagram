8da3e0f5d507bc0bf16f26d826db3043e99e9fb1
Park <winkknd@naver.com>
2019-07-08 11:03:28 +0900

    정상작동은 하지만 느림

--- src/api/Post/Files/fileControl.js
+++ src/api/Post/Files/fileControl.js
@@ -6,9 +6,11 @@ export default {
     deleteManyFiles: async (_, args, { request, isAuthenticated }) => {
       isAuthenticated(request);
       const { id, url, fileId } = args;
+      console.log(url, "이거 안되나 url");
       const result = await prisma.deleteManyFiles({
         AND: [{ url_in: url }, { id_in: fileId }]
       });
+      console.log(result, "remove file result");
     }
   }
 };
